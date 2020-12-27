8d400bad1a018be33d179a09c8413fdba600d3ee
LebedevDaniil <70822288+LebedevDaniil@users.noreply.github.com>
2020-12-27 22:27:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ document.write('<button id="search">Поиск</button>');
 var sb = document.getElementById('search');
 var url = document.getElementById('url');
 sb.addEventListener("click",function(){
-	if (url.value == "whiten://test"){
+	if (url.value == "whiten://test.com"){
 		document.location.href = "https://lebedevdaniil.github.io/test.html";
+	}
+	if (url.value == "whiten://clicker.com"){
+		document.location.href = "https://lebedevdaniil.github.io/clicker.html";
 	}
 });
