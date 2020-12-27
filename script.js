799c5f9fb3d6ddc0b705bae6d9ec839062c664b8
LebedevDaniil <70822288+LebedevDaniil@users.noreply.github.com>
2020-12-27 21:10:07 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.write('<input id="url" value="Введите название сайта:">');
+document.write('</br>');
+document.write('<button id="search">Поиск</button>');
+var sb = document.getElementById('search');
+var url = document.getElementById('url');
+sb.addEventListener("click",function(){
+	if (url.value == "whiten://test"){
+		document.location.href = "test.html";
+	}
+});
